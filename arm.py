59328ce7dc6c95ee3bb90734f60e1e0e5f119890
nimishaeden <nimmymsaju@gmail.com>
2019-03-28 05:19:08 +0530

    FIRST COMMIT

---
+++ arm.py
@@ -0,0 +1,11 @@
+x=input("enter a number")
+sum=0
+arm=x
+while(arm>0):
+      digit=arm%10
+      sum+=digit**3
+      arm//=10
+if x==sum: 
+   print(x,"is an armstrong")
+else:
+    print(x,"is not an armstrong")       
